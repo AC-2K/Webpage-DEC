a7821be7def3d68708f3e46e0c7396c6a7abe36d
AC-2K <aldair.chutumia@gmail.com>
2023-08-09 00:12:09 +0200

    adicionado comportamento do botao whatsapp

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -75,11 +75,23 @@
 
     });
 
-	
-
-	
-
-
-
-
 })(window.jQuery);
+
+//button whatsApp
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function whatsApp() {
+  location.replace("https://wa.me/258848100497");
+}
